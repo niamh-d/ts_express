d068614f14d3c199dd32e7bc08b420e53a35d618
Niamh <niamh.linguist@gmail.com>
2024-04-30 20:42:56 +0300

    patch route working

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -12,3 +12,23 @@ export const createTodo: RequestHandler = async (req, res, next) => {
 
   res.status(201).json({ message: "New todo created!", createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.status(200).json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const id = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === id);
+  if (todoIndex < 0) {
+    return res.status(404).json({ message: "Todo not found!" });
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res
+    .status(200)
+    .json({ message: "Todo updated!", updatedTodo: TODOS[todoIndex] });
+};
